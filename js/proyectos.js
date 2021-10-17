76a1fcb76b5d9183f9b743dff516e75ba1b02c38
CesarCr300 <cesarjcontreras30@gmail.com>
2021-10-17 16:01:36 -0500

    maquetacion finalizada

--- js/proyectos.js
+++ js/proyectos.js
@@ -10,7 +10,7 @@ addEventListener("DOMContentLoaded", () => {
         controlDePrecios: {
             titulo: "CONTROL DE PRECIOS",
             cliente: "Carnicería Multicarnes Santa Lucía",
-            descripcion: "Esta es una página web en la cual se pueden mostrar diversos productos para el público en la página principal. A su vez, existe la opción de que se inicie sesión, y solo un usuario con el rol de administrador es capaz de crear otro usuario.",
+            descripcion: "Página web con: CRUD, roles, autenticación, mensajes flash y autenticación por parte de los formularios.",
             enlace: "https://carniceria-sl-final.herokuapp.com/",
             tecnologiasUsadas: "JS, Node, MongoDb, Express, Bootstrap",
             enlaceGit: "https://github.com/CesarCr300/Carniceria-SL"
@@ -18,7 +18,7 @@ addEventListener("DOMContentLoaded", () => {
         apirestNotas: {
             titulo: "APIREST DE NOTAS",
             cliente: "Personal",
-            descripcion: "Esta APIREST permite la creación de un usuario la cual le brinda un token. Con este token la api permite poder crear a las notas existentes del usuario y acceder a ellas, contando con todas las funcionalidades CRUD. Por último, se realizaron los test con jest.",
+            descripcion: "Apirest con: CRUD y autenticación",
             enlace: "https://notes-apirest.herokuapp.com/",
             tecnologiasUsadas: "JS, Node, MongoDB, Express, Babel",
             enlaceGit: "https://github.com/CesarCr300/APIREST-NOTES"
