c254e8d40fafd9ee97cc895ec6edc2de5e134090
CesarCr300 <cc30122003@gmail.com>
2021-11-01 15:23:09 -0500

    layout html

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 addEventListener("DOMContentLoaded", () => {
-    const btn_menu = document.querySelector(".btn-menu")
-    if (btn_menu) {
-        btn_menu.addEventListener("click", () => {
-            const menuToDisplay = document.querySelector(".navbar-left")
-            menuToDisplay.classList.toggle("show")
-        })
-    }
-})
+  const btn_menu = document.querySelector(".btn-menu");
+  if (btn_menu) {
+    btn_menu.addEventListener("click", () => {
+      const menuToDisplay = document.querySelector(".navbar-left");
+      menuToDisplay.classList.toggle("show");
+    });
+  }
+});
