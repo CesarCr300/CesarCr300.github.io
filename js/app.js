4b321a7453b29f3a2e3fb5fa15ad991d0f544609
CesarCr300 <cesarjcontreras30@gmail.com>
2021-11-06 10:55:13 -0500

    basic structure ready

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ addEventListener("DOMContentLoaded", () => {
   const btn_menu = document.querySelector(".btn-menu");
   if (btn_menu) {
     btn_menu.addEventListener("click", () => {
-      const menuToDisplay = document.querySelector(".navbar-left");
-      menuToDisplay.classList.toggle("show");
+      console.log("HI")
+      const menuToDisplay = document.querySelector(".navbar-side-right");
+      menuToDisplay.classList.toggle("none");
     });
   }
 });
